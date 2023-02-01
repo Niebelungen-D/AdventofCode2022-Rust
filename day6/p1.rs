d83fbce911ff92162dbb8423aa183d288402c865
Niebelungen-D <dong_xr2000@163.com>
2023-02-01 16:41:19 +0800

    day6

---
+++ day6/p1.rs
@@ -0,0 +1,33 @@
+use std::collections::HashMap;
+use std::fs;
+use std::io::{self, Read};
+
+fn main() {
+    let file = fs::File::open("./input").unwrap();
+    let mut data = Vec::new();
+    let cnt = io::BufReader::new(file).read_to_end(&mut data).unwrap();
+    let mut map: HashMap<u8, usize> = HashMap::new();
+    let mut res = 0;
+    let mut left = 0;
+    
+    for right in 0..cnt {
+        if map.contains_key(&data[right]) {
+            left = match map.get(&data[right]) {
+                Some(&n) => {
+                    if n >= left {
+                        n + 1
+                    } else {
+                        left
+                    }
+                }
+                _ => left,
+            }
+        }
+        if right - left + 1 == 4 {
+            res = right + 1;
+            break;
+        }
+        map.insert(data[right], right);
+    }
+    println!("res {}", res);
+}
